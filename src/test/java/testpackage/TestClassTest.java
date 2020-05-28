328421361a905bd862db5806b70e81c74a060cbc
tmzkdl503 <tmzkdl5033@naver.com>
2020-05-28 14:14:43 +0900

    initial commit

--- src/test/java/testpackage/TestClassTest.java
+++ src/test/java/testpackage/TestClassTest.java
@@ -8,10 +8,7 @@ public class TestClassTest {
 
     @Test
     public void setValue() {
-        TestClass T = new TestClass();
-
-        T.setValue(2020);
-
-        assertEquals(2020, T.getValue());
+        TestClass.setValue(2020);
+        assertEquals(2020, TestClass.getValue());
     }
 }
