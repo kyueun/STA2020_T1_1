eff957d2cdf6fbf5563f083b259854bae8c620f0
kyueun <kelee0812.2@gmail.com>
2020-05-30 18:40:20 +0900

    Add static int Button Input in Info

--- src/main/java/testpackage/Info.java
+++ src/main/java/testpackage/Info.java
@@ -1,6 +1,8 @@
 package testpackage;
 
 public class Info {
+    final static int A = 0, B = 1, C = 2, D = 3, LONGB = 4, LONGD = 5;
+
     final static int TIMEKEEPING = 10, TIMEKEEPINGSET = 11;
     final static int TIMER = 20;
     final static int STOPWATCH = 30;
