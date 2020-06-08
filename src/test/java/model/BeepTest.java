bafda769aec0f9863716404f58caf04f0a25be63
trevel1959 <55749794+trevel1959@users.noreply.github.com>
2020-06-08 21:11:00 +0900

    Update BeepTest.java

--- src/test/java/model/BeepTest.java
+++ src/test/java/model/BeepTest.java
@@ -6,17 +6,17 @@ import view.GUI;
 import static org.junit.Assert.*;
 
 public class BeepTest {
-    GUI gui = new GUI();
-    Beep beep = new Beep(gui);
 
     @Test
     public void Beep(){
+        Beep beep = new Beep(null);
         assertEquals(1, beep.beepPopup(Info.ALARM).size());
         assertEquals(2, beep.beepPopup(Info.TIMER).size());
     }
 
     @Test
     public void MuteBeep(){
+        Beep beep = new Beep(null);
         assertEquals(1, beep.beepPopup(Info.ALARM).size());
         assertEquals(2, beep.beepPopup(Info.TIMER).size());
         assertEquals(1, beep.muteTopBeep().size());
